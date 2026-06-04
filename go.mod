9c75ed445c3baee8bbf806be952925507cb0932a
HOangAG2207 <huyhoangnguyen227@gmail.com>
2026-06-04 14:19:52 +0700

    feat: init folder structure, setup config env

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/HOangAG2207/GoBeK03Echo
+
+go 1.25.1
+
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/kelseyhightower/envconfig v1.4.0
+)
---
+++ internal/api/config.go
@@ -0,0 +1,36 @@
+package api
+
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+	"github.com/kelseyhightower/envconfig"
+)
+
+type Config struct {
+	App   AppConfig
+	Redis RedisConfig
+}
+type AppConfig struct {
+	Port        string `envconfig:"APP_PORT" default:"8081"`
+	ServiceName string `envconfig:"APP_SERVICE_NAME" default:"golang-backend-k03-with-echo-v4"`
+	InstanceID  string `envconfig:"APP_INSTANCE_ID" default:""`
+}
+type RedisConfig struct {
+	Addr     string `envconfig:"REDIS_ADDR" default:"localhost:6379"`
+	Password string `envconfig:"REDIS_PASSWORD" default:""`
+	DB       int    `envconfig:"REDIS_DB" default:"0"`
+}
+
+func NewConfig() (*Config, error) {
+	if err := godotenv.Load(); err != nil {
+		log.Println(".env not found")
+	}
+	var cfg Config
+	if err := envconfig.Process("", &cfg); err != nil {
+
+		// Nếu parse lỗi (type mismatch, thiếu field bắt buộc...)
+		return nil, err
+	}
+	return &cfg, nil
+}
