8975e3f2d32830743088dd8a8c19b12545d3bbb4
HOangAG2207 <huyhoangnguyen227@gmail.com>
2026-06-04 23:02:39 +0700

    feat: update redis client with get set value, func convert int64 to time Duration

--- pkg/redis/client.go
+++ pkg/redis/client.go
@@ -1,17 +1,39 @@
 package pkgredis
 
-import "github.com/redis/go-redis/v9"
+import (
+	"context"
+	"time"
 
-func NewClient(envPrefix string) (*redis.Client, error) {
+	"github.com/redis/go-redis/v9"
+)
+
+type Redis struct {
+	client *redis.Client
+}
+
+func NewClient(envPrefix string) (*Redis, error) {
 	cfg, err := newConfig(envPrefix)
 	if err != nil {
 		return nil, err
 	}
-	redisClient := redis.NewClient(&redis.Options{
+
+	client := redis.NewClient(&redis.Options{
 		Addr:     cfg.redis.Address,
 		Password: cfg.redis.Password,
 		DB:       cfg.redis.DB,
 	})
 
-	return redisClient, nil
+	return &Redis{client: client}, nil
+}
+func toDuration(expire int64) time.Duration {
+	if expire <= 0 {
+		return 0 // no expiration
+	}
+	return time.Duration(expire) * time.Second
+}
+func (r *Redis) SetValue(ctx context.Context, key string, value string, expire int64) error {
+	return r.client.Set(ctx, key, value, toDuration(expire)).Err()
+}
+func (r *Redis) GetValue(ctx context.Context, key string) (string, error) {
+	return r.client.Get(ctx, key).Result()
 }
